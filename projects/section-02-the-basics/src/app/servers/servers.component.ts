5628b981d4c67d3962658d8424a55714ace82301
Frank Cingel <frankhcingel@gmail.com>
2021-12-27 18:57:44 -0500

    Databinding

--- projects/section-02-the-basics/src/app/servers/servers.component.ts
+++ projects/section-02-the-basics/src/app/servers/servers.component.ts
@@ -4,18 +4,37 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',  //select creating your own element
   // selector: '[app-servers]',  //select by CSS attribute
   // selector: '.app-servers',  //select by class
-  // templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    `,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  //   `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Testserver';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  //Works but if it does not try the commented code above and below
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;      
+  }
+  // onUpdateServerName(event: Event) {
+  //   this.serverName = <HTMLInputElement>event.target.value;
+  // }
 
 }
