14e8dd85b590c39e73bea4ffbfc4816fb9d48b33
Frank Cingel <frankhcingel@gmail.com>
2021-12-30 23:37:09 -0500

    Assignment 3

---
+++ projects/assignment-03-practicing-directives/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  displayPara = true;
+  numClicks: number[] = [];
+  increment = 1;
+
+  togglePara() {
+    this.displayPara = !this.displayPara;
+    this.numClicks.push(this.increment);
+    this.increment++;
+  }
+
+  setBackground(num: number) {
+    return num >= 5 ? 'darkblue' : 'transparent';
+  }
+}
