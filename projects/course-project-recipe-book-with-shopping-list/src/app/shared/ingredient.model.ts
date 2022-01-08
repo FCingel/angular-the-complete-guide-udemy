9634ccd3bb74bd1f2a1ffe37aa8db92bf0f73944
Frank Cingel <frankhcingel@gmail.com>
2022-01-07 22:34:04 -0500

    Ingredient model

---
+++ projects/course-project-recipe-book-with-shopping-list/src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+    // Shorthand for commented below code for creating properties and assigning them in the constructor
+    constructor(public name: string, public amount: number) {}
+
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+}
