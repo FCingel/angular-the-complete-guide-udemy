7bebbf859b6464635691d37b0befbd73b1e2432e
Frank Cingel <frankhcingel@gmail.com>
2022-01-12 22:03:57 -0500

    Local References

--- projects/section-05-components-and-databinding-deep-dive/src/app/server-element/server-element.component.ts
+++ projects/section-05-components-and-databinding-deep-dive/src/app/server-element/server-element.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.Emulated           // Default so does not need to be added. Angular adds unique attributes to selectors for View Encapsulation
+  encapsulation: ViewEncapsulation.Emulated              // Default so does not need to be added. Angular adds unique attributes to selectors for View Encapsulation
   // encapsulation: ViewEncapsulation.None               // Turn off View Encapsulation (no unique attributes added)
   // encapsulation: ViewEncapsulation.ShadowDom          // uses the native Shadow DOM for the same functionality as 'Emulated' 
 })
