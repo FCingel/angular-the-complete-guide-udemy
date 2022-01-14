0bb273c8b93485f645cb707694bd34916bfbe3c4
Frank Cingel <frankhcingel@gmail.com>
2022-01-13 21:45:19 -0500

    ViewChild

--- projects/section-05-components-and-databinding-deep-dive/src/app/cockpit/cockpit.component.ts
+++ projects/section-05-components-and-databinding-deep-dive/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,8 +8,10 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
+  // @ViewChild(CockpitComponent, {static: true}) serverContentInput: ElementRef;          // can pass a component as well
 
   constructor() { }
 
@@ -17,16 +19,19 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer(nameInput: HTMLInputElement) {
+    console.log(this.serverContentInput);
+    
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
   
   onAddBlueprint(nameInput: HTMLInputElement) {
+    // this.serverContentInput.nativeElement.value = 'Something';          // Should not access the DOM like this. There are better tools like string interpolation or property binding
     this.blueprintCreated.emit({
       blueprintName: nameInput.value,
-      blueprintContent: this.newServerContent
+      blueprintContent: this.serverContentInput.nativeElement.value
     });
   }
 
