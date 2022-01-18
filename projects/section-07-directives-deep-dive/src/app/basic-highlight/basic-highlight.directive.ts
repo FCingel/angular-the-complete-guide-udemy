dedf394f0b8ca28079e0cc8d7680323849a3e581
Frank Cingel <frankhcingel@gmail.com>
2022-01-17 21:44:43 -0500

    Basic attribute directive disclaimer (not best practice)

--- projects/section-07-directives-deep-dive/src/app/basic-highlight/basic-highlight.directive.ts
+++ projects/section-07-directives-deep-dive/src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef, OnInit } from "@angular/core";
 
+// Accessing elements directly like the below example is not a best practice. Angular can render your templates without a DOM and then these properties might not be available
 @Directive({
     // Wrapped in [] to make it an attribute because then you won't have to use [] when you add appBasicHighlight to an element
     selector: '[appBasicHighlight]'
