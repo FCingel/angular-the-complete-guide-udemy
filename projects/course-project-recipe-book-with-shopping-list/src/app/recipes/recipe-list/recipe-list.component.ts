02f820f54a58c596acdf0d9124206713c6b44fef
Frank Cingel <frankhcingel@gmail.com>
2022-01-24 21:42:06 -0500

    Set up RecipeService

--- projects/course-project-recipe-book-with-shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projects/course-project-recipe-book-with-shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,18 +8,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
-  ];
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-  }
-
-  onRecipeSelected(recipe: Recipe) {    
-    this.recipeWasSelected.emit(recipe)
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ projects/course-project-recipe-book-with-shopping-list/src/app/recipes/recipe.service.ts
@@ -0,0 +1,16 @@
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
+        new Recipe('Another Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
+      ];
+
+      getRecipes() {
+          return this.recipes.slice();          // slice() with no arguments returns a copy of the array to prevent changing, since arrays are reference types in JS
+      }
+
+}
