6b13b1a5f7f473d4462aa06cd0c5918cfbbb11b2
Frank Cingel <frankhcingel@gmail.com>
2022-01-29 14:22:18 -0500

    Passing query parameters and fragments

--- projects/section-11-changing-pages-with-routing/src/app/home/home.component.ts
+++ projects/section-11-changing-pages-with-routing/src/app/home/home.component.ts
@@ -13,8 +13,8 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServer(id: number) {
     // pretend some complex calculation or reach out to backend to store something, then we want to navigate away
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});       // Programmatically add query params and fragment
   }
 }
