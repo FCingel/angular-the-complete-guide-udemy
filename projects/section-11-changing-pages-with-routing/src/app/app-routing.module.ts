3e73ff6f3dcc729bb6b048cce0fd74c5fb97ea68
Frank Cingel <frankhcingel@gmail.com>
2022-02-01 21:04:42 -0500

    Outsourcing route configuration to app-routing.module.ts

---
+++ projects/section-11-changing-pages-with-routing/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'users', component: UsersComponent, children: [     // nested routes
+        { path: ':id/:name', component: UserComponent },
+    ] },
+    { path: 'servers', component: ServersComponent, children: [
+        { path: ':id', component: ServerComponent },
+        { path: ':id/edit', component: EditServerComponent },
+    ] },
+    { path: 'not-found', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/not-found' }                   // ** is the wildcard route. This catches all routes you do not know. Must be last in list since routes are parsed from top to bottom.
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]         // exports tells Angular, if this module was added to the imports of another module, what should be accessible to that module
+})
+export class AppRoutingModule {
+
+}
