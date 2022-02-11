400116ffe30016e8de884ba6adc9d92f9739e03e
Frank Cingel <frankhcingel@gmail.com>
2022-02-11 09:46:51 -0500

    Storing subscriptions & unsubscribing

--- projects/section-13-understanding-observables/src/app/home/home.component.ts
+++ projects/section-13-understanding-observables/src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;     // store our subscription so that we can unsubscribe later and avoid memory leaks
 
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe(count => {     // similar to setInterval() where you pass in a number in ms and will fire an event every time that # of ms passes
+      console.log(count);
+    });
   }
 
+  ngOnDestroy(): void {
+      this.firstObsSubscription.unsubscribe();      // whenever we leave this component, we clear this subscription
+  }
 }
