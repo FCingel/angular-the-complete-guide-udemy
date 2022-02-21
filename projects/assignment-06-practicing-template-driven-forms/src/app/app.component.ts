6130d96149ecd7f5db4a072f2666efe31c830767
Frank Cingel <frankhcingel@gmail.com>
2022-02-21 10:04:07 -0500

    Assignment 6 complete

--- projects/assignment-06-practicing-template-driven-forms/src/app/app.component.ts
+++ projects/assignment-06-practicing-template-driven-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('formElement', {static: false}) subForm: NgForm;
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  defaultSub = "Advanced";
+  formSubmitted = false;
+  subData = {
+    email: '',
+    sub: '',
+    password: ''
+  }
 
+  onSubmit() {
+    this.subData.email = this.subForm.value.email;
+    this.subData.sub = this.subForm.value.subscription;
+    this.subData.password = this.subForm.value.password;
+    console.log(this.subForm);
+
+    this.formSubmitted = true;
+  }
 }
