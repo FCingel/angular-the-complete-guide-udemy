783c010882368bb06fac966e8a5b5a685818b269
Frank Cingel <frankhcingel@gmail.com>
2022-02-23 21:39:41 -0500

    Creating a custom asynchronous validator

--- projects/section-15-reactive-forms/src/app/app.component.ts
+++ projects/section-15-reactive-forms/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,7 @@ export class AppComponent implements OnInit{
       userData: new FormGroup({
         // forbiddenUsernames validator must use bind since this class does not call it. Angular calls it when it checks validity & during that time 'this' will not refer to this class
         username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),      // no need to bind 'this' since it is not used in Validator method
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([])
@@ -51,5 +52,19 @@ export class AppComponent implements OnInit{
       return {'nameIsForbidden': true};     // error nameIsForbidden is true
     }
     return null;      // return null if valid. Do not return {'nameIsForbidden': false}.
+  }
+
+  // Async validator. setTimeout simulates a request taking some time.
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
