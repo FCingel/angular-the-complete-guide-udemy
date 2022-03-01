a68cec21693978e7a8ae526a50f8e0f9267befd0
Frank Cingel <frankhcingel@gmail.com>
2022-02-28 23:12:46 -0500

    Assignment 7 complete

--- projects/assignment-07-practicing-reactive-forms/src/app/app.component.ts
+++ projects/assignment-07-practicing-reactive-forms/src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  statuses = ['Stable', 'Critical', 'Finished'];
+  reactiveSignupForm: FormGroup;
+
+  ngOnInit(): void {
+      this.reactiveSignupForm = new FormGroup({
+        projectName: new FormControl(null, [Validators.required, CustomValidators.projectNameValidator], CustomValidators.asyncProjectNameValidator),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        status: new FormControl(this.statuses[1])
+      })
+  }
+
+  onSubmit() {
+    console.log(this.reactiveSignupForm);
+  }
 }
